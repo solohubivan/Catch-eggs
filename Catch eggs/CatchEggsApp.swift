e0676a6af1e14de816a71f99bb702acc139ad92d
solohubivan <solohubivan@gmail.com>
2025-12-15 22:12:29 +0200

    Initial Commit

---
+++ Catch eggs/CatchEggsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Catch_eggsApp.swift
+//  Catch eggs
+//
+//  Created by Ivan Solohub on 15.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CatchEggsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            GameScreenView()
+        }
+    }
+}
